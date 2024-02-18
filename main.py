fe68be3c43caeb58ad6281ad54263760edfb9531
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-02-18 16:33:12 +0700

    Main block

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import tkinter
-import tkinter as tk
-from tkinter import ttk
-from keypad import Keypad
+"""Main block to run the program."""
+from calculator_sys import CalculatingSystem, HistorySystem
+from calculator_ui import CalculatorUI
+from controller import Controller
 
 
-print('Test')
+history_sys = HistorySystem()
+calculator = CalculatingSystem(history_sys)
+controller = Controller(calculator)
+ui = CalculatorUI(controller)
+ui.run()
