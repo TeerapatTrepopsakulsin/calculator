4883a71e08a5c577e4e590048b98a43aeff3e360
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-02-18 16:53:03 +0700

    Add docstring

--- math_functions.py
+++ math_functions.py
@@ -1,9 +1,14 @@
+"""Enum class of Mathematics functions"""
 from enum import Enum
-from math import *
 
 
 class MathOperators(Enum):
-    # FUNCTION = [for button, in label, for eval]
+    """Enum class which store the expression of operators in difference use as a list,
+    index 0 is shown on the button,
+    index 1 is shown in the label
+    and index 2 is use for evaluation.
+    """
+    # OPERATOR = [for button, in label, for eval]
     O_PAREN = ['(', '(', '(']
     C_PAREN = [')', ')', ')']
     MUL = ['*', '*', '*']
@@ -22,13 +27,20 @@ class MathOperators(Enum):
 
     @staticmethod
     def get(item):
+        """Return a list of index 1 and to if item is index 0"""
         for _function in MathOperators:
             if _function.value[0] == item:
                 return _function.value[1:]
+        return None
 
 
 class MathFunctions(Enum):
-    # FUNCTION = [for button, in label, for eval]
+    """Enum class which store the expression of operators in difference use as a list,
+    index 0 is shown in the combobox,
+    index 1 is shown in the label
+    and index 2 is use for evaluation.
+    """
+    # FUNCTION = [for combobox, in label, for eval]
     EXP = ['exp', 'exp(', 'exp(']
     LN = ['ln', 'ln(', 'log(']
     LOG10 = ['log10', 'log10(', 'log10(']
@@ -44,6 +56,8 @@ class MathFunctions(Enum):
 
     @staticmethod
     def get(item):
+        """Return a list of index 1 and to if item is index 0"""
         for _function in MathFunctions:
             if _function.value[0] == item:
                 return _function.value[1:]
+        return None
